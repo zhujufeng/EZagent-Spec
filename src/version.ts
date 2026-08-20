82b24195e35212e62bb2265754486c31622bea7f
zhujufeng <2399642396@qq.com>
2026-08-20 17:01:23 +0800

    build: scaffold TypeScript local core

---
+++ src/version.ts
@@ -0,0 +1,1 @@
+export const EZAGENT_VERSION = "0.1.0" as const;
---
+++ test/version.test.ts
@@ -0,0 +1,7 @@
+import { EZAGENT_VERSION } from "../src/version.js";
+
+import { expect, test } from "vitest";
+
+test("exports the package version", () => {
+  expect(EZAGENT_VERSION).toBe("0.1.0");
+});
